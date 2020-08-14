ceefbe44de379fb1fc560b4a0541fd27e8539004
Rishabh Kumar <me.rishabh20@gmail.com>
2020-08-14 16:14:32 +0530

    website enhanced

--- src/contact/models.py
+++ src/contact/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ContactDetails(models.Model):
-    #location =  
+    location = models.CharField(max_length=300, default='')
     email = models.EmailField() 
     phone_no = models.CharField(max_length=15)
 
